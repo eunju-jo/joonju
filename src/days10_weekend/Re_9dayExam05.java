9fab6cb175fe8408dad719a1e3fbcc903a1455fc
조은주 <eeeunjujo@gmail.com>
2021-03-21 23:31:48 +0900

    9일 문제 다시 복습차 풀이

--- src/days10_weekend/Re_9dayExam05.java
+++ src/days10_weekend/Re_9dayExam05.java
@@ -1,5 +1,7 @@
 package days10_weekend;
 
+import java.util.Scanner;
+
 /**
  * @author 조은주
  * @date Mar 21, 2021 - 3:24:05 AM
@@ -16,6 +18,64 @@ public class Re_9dayExam05 {
 //	    세기 (1800년대 , 1900년대,  2000년대 )  :  getCentry() 메서드 선언
 //	    후 출력하는 코딩.
 		
+		
+		String rrn;
+		Scanner scanner = new Scanner(System.in);
+		String regex = "\\d{6}-\\d{7}";
+		boolean flag = false;
+		
+		do {
+			if(flag) System.out.println("입력오류. 양식에 맞게 다시 입력하세요.");
+			System.out.print("주민번호를 입력하세요(예:840218-2140525) : ");
+			rrn = scanner.nextLine();
+			
+		} while (flag = !rrn.matches(regex));
+		
+		if( getGender(rrn)) System.out.println("남자");
+		else System.out.println("여자");
+
+		isForeigner(rrn);
+		if(isForeigner(rrn)) System.out.println("외국인");
+		else System.out.println("내국인");
+		
+		
+		System.out.printf("%d년대", getCentury(rrn));
+		
+		
+		
+	}//main
+
+	private static int getCentury(String rrn) {
+		int genderNum = rrn.charAt(7)-48;
+		int century;
+		switch (genderNum) {
+		case 9: case 0:
+			century = 1800;
+			break;
+
+		case 1: case 2: case 5: case 6:
+			century = 1900;
+			break;
+
+		default:
+			century = 2000;
+			break;
+		}
+		
+		return century;
+		
 	}
 
-}
+	private static boolean isForeigner(String rrn) {
+		int genderNum = rrn.charAt(7)-48;
+		if(5<=genderNum && genderNum <=8) return true;
+		else return false;
+	}
+
+	private static boolean getGender(String rrn) {
+		int genderNum = rrn.charAt(7)-48;
+		if(genderNum %2 != 0) return true;
+		else return false;
+	}
+
+}//class
--- src/days10_weekend/Re_9dayExam06.java
+++ src/days10_weekend/Re_9dayExam06.java
@@ -1,13 +1,73 @@
 package days10_weekend;
 
+import java.util.Random;
+import java.util.Scanner;
+
+/**
+ * @author 조은주
+ * @date Mar 21, 2021 - 3:24:02 AM
+ * @subject
+ * @content
+ *
+ */
 public class Re_9dayExam06 {
 
 	public static void main(String[] args) {
-//		6.  문제1번에서 찾을 정수(n) 이 여러 개인 경우 찾을 정수(n) 이 있는 모든 위치를 구해서 출력하세요.
-//		  예)  int [] m =  [1] [20] [2] [20] [3] [20] [4] [5] [6] [7]
-//		        n = 20
-//		        이라고 가정하면 
-//		        찾는 위치는             1            3         5   라고 출력.....
+		//		6.  문제1번에서 찾을 정수(n) 이 여러 개인 경우 찾을 정수(n) 이 있는 모든 위치를 구해서 출력하세요.
+		//		  예)  int [] m =  [1] [20] [2] [20] [3] [20] [4] [5] [6] [7]
+		//		        n = 20
+		//		        이라고 가정하면 
+		//		        찾는 위치는             1            3         5   라고 출력.....
+		int [] m = new int[10];
+
+		getNum(m);
+
+
+
+	}//main
+
+	private static void getNum(int[] m) {
+		Random rnd = new Random();
+		//m[idx] = n;
+		int idx = 0;
+		int n;
+		while (idx<m.length) {
+			n = rnd.nextInt(30)+1;
+			m[idx] = n;
+			idx++;
+			System.out.printf("[%d]", n);
+			
+			
+			if(idx==m.length) findNum(m,n,idx);
+		}
+		
+	
 	}
 
-}
+	private static void findNum(int[] m, int n, int idx) {
+		System.out.println("찾고자 하는 수를 입력하세요: ");
+		Scanner scanner = new Scanner(System.in);
+		int wants = scanner.nextInt();
+	
+		
+		for (int i = 0; i < m.length; i++) {
+			idx = isWantNum(m, wants, i);
+			if(wants == m[i]) {
+				System.out.printf("%d는 %d번째 자리에 있습니다.\n" , wants, i+1);
+			}
+		}
+		
+		if(idx== -1) System.out.println("찾으시는 수는 본 배열에 없습니다.");
+		//찾으시는~ 문장 반복적으로 출력되지 않게 하도록 반복문 밖으로 빼고, idx를 isWantNum 메서드로 빼서 -1로 받음
+	}
+
+	private static int isWantNum(int [] m, int wants, int i) {
+
+		for (int j = 0; j < m.length; j++) {
+			if(wants == m[j]) return j;
+		}
+		
+		return -1;
+	}
+
+}//class
